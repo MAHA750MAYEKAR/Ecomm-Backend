e95801a00ecd6ad75eda5de4347b1c54787e5534
MAHANANDA MAYEKAR <mayekardiksha750@gmail.com>
2024-12-19 10:26:01 +0530

    added user scema

---
+++ SRC/Schema/userSchema.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose'
+
+const UserSchema=new mongoose.Schema({
+      name:{
+            type:String,
+            required:[true,"please enter your name"],
+            minLength:5
+      },
+      password:{
+            type:String,
+            required:[true,"Please enter your password"]
+      },
+      email:{
+            type:String,
+            unique:[true,"This email already exist"],
+            required:[true,"Please enter your email"],
+            match:[/^[\w.-]+@([\w-]+\.)+[a-zA-Z]{2,}$/,"Please enter valid email "],
+            lowercase:true
+      },
+      userTypes:{
+            type:String,
+            required:true,
+            default:"customer",
+            enum:["customer","admin"]//what is enum
+      }
+})
+
+export const User=mongoose.model("User",UserSchema)
